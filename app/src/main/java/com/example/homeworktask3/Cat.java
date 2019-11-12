5920dd97d88e41483f32fd5610c6b1eca019bfa0
Joanna Damianakis <z5162567@ad.unsw.edu.au>
2019-11-12 11:59:30 +1100

    Initial commit

---
+++ app/src/main/java/com/example/homeworktask3/Cat.java
@@ -0,0 +1,27 @@
+package com.example.homeworktask3;
+
+import androidx.annotation.NonNull;
+import androidx.room.PrimaryKey;
+
+public class Cat {
+
+    @PrimaryKey
+    @NonNull
+    private String id;
+    private String name;
+
+    //TODO: find description in api
+    private String description;
+
+    private String weight_imperial;
+    private String temperament;
+    private String origin;
+    private String life_span;
+    private int dog_friendly;
+    private String wikipedia_url;
+
+    ///sdkfjhdsf
+
+
+
+}
---
+++ app/src/main/java/com/example/homeworktask3/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.homeworktask3;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = new SearchFragment();
+        swapFragment(fragment);
+
+        bottomNavigationView = findViewById(R.id.nav_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.searchButton) {
+                    Fragment fragment = new SearchFragment();
+                    swapFragment(fragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.favouritesButton) {
+                    Fragment fragment = new FavouritesFragment();
+                    swapFragment(fragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private void swapFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_slot, fragment);
+        fragmentTransaction.commit();
+    }
+}
